97f956a023bee1b5a2d243b4c43ae1a3a0aa9c6c
LuciaRyan <lcryan06@gmail.com>
2022-11-15 12:01:47 +0100

    Added second and third article to webpage according to the assignment

--- src/App.js
+++ src/App.js
@@ -14,15 +14,36 @@ function App() {
                     eten.
                     Van citroen kun je het vruchtvlees, het sap en de schil gebruiken.
                     Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel.</p>
+
+                <h2 className="product-name">Limoen</h2>
                 <article className="product">
                     <img
                         src="https://thumbs.dreamstime.com/b/lemon-isolated-24834437.jpg"
                         alt="Citroen"
                     />
+                    <p className="product-description">
+                        Limoen
+                        Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten
+                        (Wijnruitfamilie).
+                        Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen.
+                    </p>
 
+                    <h2 className="product-name">IJsblokjes</h2>
+                    <article className="product">
+                        <img
+                            src="https://thumbs.dreamstime.com/b/lemon-isolated-24834437.jpg"
+                            alt="Citroen"
+                        />
+                        <p className="product-description">
+                            Limoen
+                            Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten
+                            (Wijnruitfamilie).
+                            Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen.
+                        </p>
+
+                    </article>
                 </article>
             </article>
-
         </main>
     );
 }
